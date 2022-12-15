1cb56d0715aa91e8986780753cde10c5849c6ccc
thomas <tom.angotti11@gmail.com>
2022-12-15 10:08:14 -0700

    commit tree 8aba9adc63dead80e889d8e07485c355070f9a3d

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+;
+
+function writeCards(names, event) {
+    let message = [];
+    let i = 0;
+    while(i < names.length){
+        message[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        i++;
+    }
+    return message;
+}
+
+console.log(writeCards(['Charlie', 'Samip', 'Ali'], "birthday"));
+
+function countDown(num1) {
+    let i = 0;
+    while (i <= num1){
+        console.log(num1 - i);
+        i++;
+    };
+}
+
+countDown(15)
